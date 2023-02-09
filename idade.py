10e828e3df431b23ac39e36f4f3efd23a0eebf3b
Ian Gandine <iangandine@gmail.com>
2023-02-09 17:15:41 -0300

    exercicio revisao prog 2

---
+++ idade.py
@@ -0,0 +1,13 @@
+
+def main():
+    idade=int(input())
+    npessoas=0
+    idadegeral=0
+    while idade!=0:
+        npessoas=npessoas+1
+        idadegeral=idadegeral+idade
+        idade=int(input())
+    print(idadegeral/npessoas)
+    
+if __name__ == '__main__':
+    main()
