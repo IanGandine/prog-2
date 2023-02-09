30752b800d403e241e06bfbd5e64c5aeacfa07a4
Ian Gandine <iangandine@gmail.com>
2023-02-09 17:34:29 -0300

    commit teste

--- idade.py
+++ idade.py
@@ -1,15 +1,1 @@
-
-def main():
-    idade=int(input())
-    npessoas=0
-    idadegeral=0
-    while idade!=0:
-        npessoas=npessoas+1
-        idadegeral=idadegeral+idade
-        idade=int(input())
-    print(idadegeral/npessoas)
-    
-if __name__ == '__main__':
-    main()
-
-#coment
+#addzsadadsasd
